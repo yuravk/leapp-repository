9163f4db4ff424e0cf7dbcffdede6f0f3f3168ad
Andrew Lukoshko <andrew.lukoshko@gmail.com>
2021-09-27 20:20:33 +0300

    Use ELevate name in GRUB entries
    (cherry picked from commit 66fe44ed2c92ee3a0ae0cabe50df9fb5bdbb8d1f)

--- repos/system_upgrade/common/actors/addupgradebootentry/tests/unit_test_addupgradebootentry.py
+++ repos/system_upgrade/common/actors/addupgradebootentry/tests/unit_test_addupgradebootentry.py
@@ -42,7 +42,7 @@ run_args_add = [
     '/usr/sbin/grubby',
     '--add-kernel', '/abc',
     '--initrd', '/def',
-    '--title', 'AlmaLinux-Upgrade-Initramfs',
+    '--title', 'ELevate-Upgrade-Initramfs',
     '--copy-default',
     '--make-default',
     '--args',
