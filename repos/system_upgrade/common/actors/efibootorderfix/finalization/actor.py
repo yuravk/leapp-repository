e4ce70da849f09c811ba59bcf857614200d4eaa7
Jason Tucker <jasonwtucker@gmail.com>
2022-10-03 18:20:40 +0000

    Modify efibootorderfix to support distro changes (#3)
    * Modify efibootorderfix to support distro changes
    
    almalinux/leapp-repository supports upgrades to different OS
    distributions (e.g. CentOS 7 -> Rocky 8); however the
    efibootorderfix (finalization) actor only assumes you are
    upgrading to the same distro (e.g. Redhat 7 -> Redhat 8). This
    causes first boot to fail on UEFI-enabled hardware post-upgrade.
    This patch makes the EFI boot entry configuration more flexible
    to support distro changes.
    
    * Add arch detection and get release from system-release
    
    * Remove release version from the boot entry label
    
    As per request from @andrewlukoshko.
    
    * EfiFinalizationFix noop if not has_efibootmgr
    
    Co-authored-by: Roman Prilipskii <rprilipskii@cloudlinux.com>

--- repos/system_upgrade/common/actors/efibootorderfix/finalization/actor.py
+++ repos/system_upgrade/common/actors/efibootorderfix/finalization/actor.py
@@ -1,17 +1,78 @@
+import os
+
+from leapp.libraries.stdlib import run, api
 from leapp.actors import Actor
-from leapp.libraries.common import efi_reboot_fix
+from leapp.models import InstalledTargetKernelVersion, KernelCmdlineArg, FirmwareFacts, MountEntry
 from leapp.tags import FinalizationPhaseTag, IPUWorkflowTag
+from leapp.exceptions import StopActorExecutionError
 
 
 class EfiFinalizationFix(Actor):
     """
-    Adjust EFI boot entry for final reboot
+    Ensure that EFI boot order is updated, which is particularly necessary
+    when upgrading to a different OS distro. Also rebuilds grub config
+    if necessary.
     """
 
     name = 'efi_finalization_fix'
-    consumes = ()
+    consumes = (KernelCmdlineArg, InstalledTargetKernelVersion, FirmwareFacts, MountEntry)
     produces = ()
     tags = (FinalizationPhaseTag, IPUWorkflowTag)
 
     def process(self):
-        efi_reboot_fix.maybe_emit_updated_boot_entry()
+        is_system_efi = False
+        ff = next(self.consume(FirmwareFacts), None)
+
+        dirname = {
+                'AlmaLinux': 'almalinux',
+                'CentOS Linux': 'centos',
+                'CentOS Stream': 'centos',
+                'Oracle Linux Server': 'redhat',
+                'Red Hat Enterprise Linux': 'redhat',
+                'Rocky Linux': 'rocky'
+        }
+
+        efi_shimname_dict = {
+            'x86_64': 'shimx64.efi',
+            'aarch64': 'shimaa64.efi'
+        }
+
+        with open('/etc/system-release', 'r') as sr:
+            release_line = next(line for line in sr if 'release' in line)
+            distro = release_line.split(' release ', 1)[0]
+
+        efi_bootentry_label = distro
+        distro_dir = dirname.get(distro, 'default')
+        shim_filename = efi_shimname_dict.get(api.current_actor().configuration.architecture, 'shimx64.efi')
+
+        shim_path = '/boot/efi/EFI/' + distro_dir + '/' + shim_filename
+        grub_cfg_path = '/boot/efi/EFI/' + distro_dir + '/grub.cfg'
+        bootmgr_path = '\\EFI\\' + distro_dir + '\\' + shim_filename
+
+        has_efibootmgr = os.path.exists('/sbin/efibootmgr')
+        has_shim = os.path.exists(shim_path)
+        has_grub_cfg = os.path.exists(grub_cfg_path)
+
+        if not ff:
+            raise StopActorExecutionError(
+                'Could not identify system firmware',
+                details={'details': 'Actor did not receive FirmwareFacts message.'}
+            )
+
+        if not has_efibootmgr:
+            return
+
+        for fact in self.consume(FirmwareFacts):
+            if fact.firmware == 'efi':
+                is_system_efi = True
+                break
+
+        if is_system_efi and has_shim:
+            with open('/proc/mounts', 'r') as fp:
+                for line in fp:
+                    if '/boot/efi' in line:
+                        efidev = line.split(' ', 1)[0]
+            run(['/sbin/efibootmgr', '-c', '-d', efidev, '-p 1', '-l', bootmgr_path, '-L', efi_bootentry_label])
+
+            if not has_grub_cfg:
+                run(['/sbin/grub2-mkconfig', '-o', grub_cfg_path])
