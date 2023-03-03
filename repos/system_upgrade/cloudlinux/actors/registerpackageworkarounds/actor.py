0d71968317d84e4bc48a762386f457ae16d0842b
Roman Prilipskii <prilr@protonmail.com>
2023-03-03 05:07:33 -0500

    Extend DNF package workaround to have two lists - to reinstall and to delete (#49)
    * Add cloudlinux-fchange to problem packages
    
    * Make cloudlinux-fchange not reinstallable
    
    * Remove cloudlinux-fchange from problem packages, keep structural changes
    
    * Fix incorrect logger invocation

--- repos/system_upgrade/cloudlinux/actors/registerpackageworkarounds/actor.py
+++ repos/system_upgrade/cloudlinux/actors/registerpackageworkarounds/actor.py
@@ -1,13 +1,8 @@
 from leapp.actors import Actor
+from leapp.libraries.actor import registerpackageworkarounds
 from leapp.models import InstalledRPM, DNFWorkaround, PreRemovedRpmPackages
 from leapp.tags import FactsPhaseTag, IPUWorkflowTag
 from leapp.libraries.common.cllaunch import run_on_cloudlinux
-
-# NOTE: The related packages are listed both here *and* in the workaround script!
-# If the list changes, it has to change in both places.
-# This is a limitation of the current DNFWorkaround implementation.
-# TODO: unify the list in one place. A separate common file, perhaps?
-PACKAGE_LIST = ['gettext-devel']
 
 
 class RegisterPackageWorkarounds(Actor):
@@ -23,22 +18,4 @@ class RegisterPackageWorkarounds(Actor):
 
     @run_on_cloudlinux
     def process(self):
-        preremoved_pkgs = PreRemovedRpmPackages(install=True)
-        # Only produce a message if a package is actually about to be uninstalled
-        for rpm_pkgs in self.consume(InstalledRPM):
-            for pkg in rpm_pkgs.items:
-                if (pkg.name in PACKAGE_LIST):
-                    preremoved_pkgs.items.append(pkg)
-                    self.log.debug("Listing package {} to be pre-removed".format(pkg.name))
-
-        if preremoved_pkgs.items:
-            self.produce(preremoved_pkgs)
-
-        self.produce(
-            # yum doesn't consider attempting to remove a non-existent package to be an error
-            # we can safely give it the entire package list without checking if all are installed
-            DNFWorkaround(
-                display_name='problem package modification',
-                script_path=self.get_tool_path('remove-problem-packages'),
-            )
-        )
+        registerpackageworkarounds.process()
---
+++ repos/system_upgrade/cloudlinux/actors/registerpackageworkarounds/libraries/registerpackageworkarounds.py
@@ -0,0 +1,38 @@
+from leapp.actors import Actor
+from leapp.models import InstalledRPM, DNFWorkaround, PreRemovedRpmPackages
+from leapp.libraries.stdlib import api
+
+# NOTE: The related packages are listed both here *and* in the workaround script!
+# If the list changes, it has to change in both places.
+# This is a limitation of the current DNFWorkaround implementation.
+# TODO: unify the list in one place. A separate common file, perhaps?
+TO_REINSTALL = ['gettext-devel']  # These packages will be marked for installation
+TO_DELETE = []  # These won't be
+
+
+def produce_workaround_msg(pkg_list, reinstall):
+    if not pkg_list:
+        return
+    preremoved_pkgs = PreRemovedRpmPackages(install=reinstall)
+    # Only produce a message if a package is actually about to be uninstalled
+    for rpm_pkgs in api.consume(InstalledRPM):
+        for pkg in rpm_pkgs.items:
+            if (pkg.name in pkg_list):
+                preremoved_pkgs.items.append(pkg)
+                api.current_logger().debug("Listing package {} to be pre-removed".format(pkg.name))
+    if preremoved_pkgs.items:
+        api.produce(preremoved_pkgs)
+
+
+def process():
+    produce_workaround_msg(TO_REINSTALL, True)
+    produce_workaround_msg(TO_DELETE, False)
+
+    api.produce(
+        # yum doesn't consider attempting to remove a non-existent package to be an error
+        # we can safely give it the entire package list without checking if all are installed
+        DNFWorkaround(
+            display_name='problem package modification',
+            script_path=api.get_tool_path('remove-problem-packages'),
+        )
+    )
