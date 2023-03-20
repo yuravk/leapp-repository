ad15a112d0e5b9b2dad12ccb2a65f9b018c848b1
Roman Prilipskii <prilr@protonmail.com>
2023-03-20 11:28:01 -0400

    Extend the rpmnew replacement actor (#61)
    * Make two repo rpmnew handlers - to erase and to backup
    
    * Log altered files
    
    * Formatting edit

--- repos/system_upgrade/cloudlinux/actors/replacerpmnewconfigs/actor.py
+++ repos/system_upgrade/cloudlinux/actors/replacerpmnewconfigs/actor.py
@@ -3,12 +3,14 @@ import os
 import fileinput
 
 from leapp.actors import Actor
-from leapp.tags import FirstBootPhaseTag, IPUWorkflowTag
+from leapp.tags import ApplicationsPhaseTag, IPUWorkflowTag
 from leapp import reporting
+from leapp.reporting import Report
 from leapp.libraries.common.cllaunch import run_on_cloudlinux
 
 REPO_DIR = '/etc/yum.repos.d'
-CL_MARKERS = ['cloudlinux', 'imunify']
+REPO_DELETE_MARKERS = ['cloudlinux', 'imunify', 'epel']
+REPO_BACKUP_MARKERS = []
 RPMNEW = '.rpmnew'
 LEAPP_BACKUP_SUFFIX = '.leapp-backup'
 
@@ -20,16 +22,27 @@ class ReplaceRpmnewConfigs(Actor):
 
     name = 'replace_rpmnew_configs'
     consumes = ()
-    produces = ()
-    tags = (FirstBootPhaseTag, IPUWorkflowTag)
+    produces = (Report,)
+    tags = (ApplicationsPhaseTag, IPUWorkflowTag)
 
     @run_on_cloudlinux
     def process(self):
+        deleted_repofiles = []
         renamed_repofiles = []
 
         for reponame in os.listdir(REPO_DIR):
-            if any(mark in reponame for mark in CL_MARKERS) and RPMNEW in reponame:
-                base_reponame = reponame[:-7]
+            if any(mark in reponame for mark in REPO_DELETE_MARKERS) and RPMNEW in reponame:
+                base_reponame = reponame[:-len(RPMNEW)]
+                base_path = os.path.join(REPO_DIR, base_reponame)
+                new_file_path = os.path.join(REPO_DIR, reponame)
+
+                os.unlink(base_path)
+                os.rename(new_file_path, base_path)
+                deleted_repofiles.append(base_reponame)
+                self.log.debug('Yum repofile replaced: {}'.format(base_path))
+
+            if any(mark in reponame for mark in REPO_BACKUP_MARKERS) and RPMNEW in reponame:
+                base_reponame = reponame[:-len(RPMNEW)]
                 base_path = os.path.join(REPO_DIR, base_reponame)
                 new_file_path = os.path.join(REPO_DIR, reponame)
                 backup_path = os.path.join(REPO_DIR, base_reponame + LEAPP_BACKUP_SUFFIX)
@@ -37,6 +50,7 @@ class ReplaceRpmnewConfigs(Actor):
                 os.rename(base_path, backup_path)
                 os.rename(new_file_path, base_path)
                 renamed_repofiles.append(base_reponame)
+                self.log.debug('Yum repofile replaced with backup: {}'.format(base_path))
 
         # Disable any old repositories.
         for reponame in os.listdir(REPO_DIR):
@@ -48,17 +62,19 @@ class ReplaceRpmnewConfigs(Actor):
                     else:
                         print(line, end='')
 
-        if renamed_repofiles:
-            replaced_string = '\n'.join(['- {}'.format(repofile_name) for repofile_name in renamed_repofiles])
+        if renamed_repofiles or deleted_repofiles:
+            deleted_string = '\n'.join(['{}'.format(repofile_name) for repofile_name in deleted_repofiles])
+            replaced_string = '\n'.join(['{}'.format(repofile_name) for repofile_name in renamed_repofiles])
             reporting.create_report([
                 reporting.Title('CloudLinux repository config files replaced by updated versions'),
                 reporting.Summary(
-                    'One or more RPM repository configuration files related to CloudLinux '
+                    'One or more RPM repository configuration files '
                     'have been replaced with new versions provided by the upgraded packages. '
                     'Any manual modifications to these files have been overriden by this process. '
-                    'Old versions of files were backed up to files with a naming pattern '
+                    'Old versions of backed up files are contained in files with a naming pattern '
                     '<repository_file_name>.leapp-backup. '
-                    'Replaced repository files: \n{}'.format(replaced_string)
+                    'Deleted repository files: \n{}\n'
+                    'Backed up repository files: \n{}'.format(deleted_string, replaced_string)
                 ),
                 reporting.Severity(reporting.Severity.MEDIUM),
                 reporting.Tags([reporting.Tags.UPGRADE_PROCESS])
