68caea97aab86aee6d5cb5c99f4faea16248b772
Evgeni Golov <evgeni@golov.de>
2024-05-27 11:58:46 +0200

    refactor distributionsignedrpmscanner to use common distro helper
    (cherry picked from commit f822cb805ed9efd101fd212eb162544ab29c8fe9)

--- repos/system_upgrade/common/libraries/distro.py
+++ repos/system_upgrade/common/libraries/distro.py
@@ -3,6 +3,7 @@ import os
 
 from leapp.exceptions import StopActorExecutionError
 from leapp.libraries.stdlib import api
+from leapp.models import VendorSignatures
 
 
 def get_distribution_data(distribution):
@@ -11,8 +12,14 @@ def get_distribution_data(distribution):
     distribution_config = os.path.join(distributions_path, distribution, 'gpg-signatures.json')
     if os.path.exists(distribution_config):
         with open(distribution_config) as distro_config_file:
-            return json.load(distro_config_file)
+            distro_config_json = json.load(distro_config_file)
     else:
         raise StopActorExecutionError(
             'Cannot find distribution signature configuration.',
             details={'Problem': 'Distribution {} was not found in {}.'.format(distribution, distributions_path)})
+
+    # Extend with Vendors signatures
+    for siglist in api.consume(VendorSignatures):          
+        distro_config_json["keys"].extend(siglist.sigs)
+
+    return distro_config_json
