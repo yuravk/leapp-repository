fb72d91b21a2ec8927086bc968bbb36ca18bba98
Matej Matuska <mmatuska@redhat.com>
2025-07-08 10:03:30 +0200

    Remove deprecated InstalledRedHatSignedRPM
    This has been deprecated and replaced by DistributionSignedRPM since
    2024-01-31.
    
    (cherry picked from commit 846057b84526d972bd639af960ded8145abcf1d5)

--- repos/system_upgrade/common/actors/distributionsignedrpmscanner/actor.py
+++ repos/system_upgrade/common/actors/distributionsignedrpmscanner/actor.py
@@ -1,11 +1,10 @@
 from leapp.actors import Actor
 from leapp.libraries.actor import distributionsignedrpmscanner
-from leapp.models import DistributionSignedRPM, InstalledRedHatSignedRPM, InstalledRPM, InstalledUnsignedRPM, VendorSignatures
+from leapp.models import DistributionSignedRPM, InstalledRPM, InstalledUnsignedRPM, VendorSignatures
 from leapp.tags import FactsPhaseTag, IPUWorkflowTag
 from leapp.utils.deprecation import suppress_deprecation
 
 
-@suppress_deprecation(InstalledRedHatSignedRPM)
 class DistributionSignedRpmScanner(Actor):
     """
     Provide data about distribution plus vendors signed & unsigned RPM packages.
@@ -34,7 +33,7 @@ class DistributionSignedRpmScanner(Actor):
 
     name = 'distribution_signed_rpm_scanner'
     consumes = (InstalledRPM, VendorSignatures)
-    produces = (DistributionSignedRPM, InstalledRedHatSignedRPM, InstalledUnsignedRPM,)
+    produces = (DistributionSignedRPM, InstalledUnsignedRPM)
     tags = (IPUWorkflowTag, FactsPhaseTag)
 
     def process(self):
